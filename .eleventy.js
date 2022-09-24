4afd90aa59f6f862bede45087c28547958a10d4b
Mark Tomlinson <mark.tomlinson3@gmail.com>
2022-09-24 13:32:50 +1000

    Use pug not nunjucks

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
+const pug = require('pug')
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('./src/scss/')
+  eleventyConfig.addWatchTarget('./src/styles/')
   eleventyConfig.addWatchTarget('./src/js/')
+  eleventyConfig.htmlTemplateEngine = 'pug'
+  eleventyConfig.setLibrary('pug', pug)
+  eleventyConfig.setTemplateFormats(['pug', 'md'])
+
   return {
     // configuration object for directories
     dir: {
--- src/pages/index.md
+++
@@ -1,9 +0,0 @@
----
-title: Home Page
-description: This is the home page. It isn't very interesting right now.
-layout: layout.njk
----
-
-# Home Page
-
-Hello World!
